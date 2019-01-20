23112b5cc28bb207d8643731be8df6c76f559b56
yupan <fish>
2019-01-20 16:50:03 +0800

    Move to new repository

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@
 from setuptools import setup, find_packages
 
 __author__ = "FishBone"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 setup(name='managpu',
       version=__version__,
       description='managpu: for choosing gpus',
       author=__author__,
       maintainer=__author__,
-      url='https://github.com/DogfishBone/GpuManager',
+      url='https://github.com/DogfishBone/managpu',
       packages=find_packages(),
       py_modules=[],
       long_description="Make setting gpus more easy.",
-      license="Public",
+      license="GPLv3",
       platforms=["any"],
 )
